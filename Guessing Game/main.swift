27b8b24866bf94075f46af82ba64aaa606b12f7e
Cristian Rodriguez <cristianrodriguez@Interapts-MacBook-Pro-9.local>
2018-01-24 13:23:26 -0500

    outlined project

--- Guessing Game/main.swift
+++ Guessing Game/main.swift
@@ -8,5 +8,49 @@
 
 import Foundation
 
+//
+
+
+for o in 1...5 {
+    let correct = Int(arc4random_uniform(100))
+    var lastGuess = 0
+        for i in 1...5 {
+            print("Guess #\(i):?")
+            let guess = Int(readLine()!)
+            var out = ""
+            if correct > 100 {
+                out = "More"
+            }
+            if correct < 100 {
+                out = "Less"
+            }
+            if correct == correct  {
+                out += " Slightly"
+            }
+            else {
+                out += " Alot"
+            }
+            if correct == guess {
+                print("Got it")
+                break
+            }
+    }
+    print("Would you like to play again??")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
