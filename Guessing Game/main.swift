410462948699d1da3c24ad09f347d5f04f0b367a
Cristian Rodriguez <cristianrodriguez@Interapts-MacBook-Pro-9.local>
2018-01-25 13:24:56 -0500

    got a working game

--- Guessing Game/main.swift
+++ Guessing Game/main.swift
@@ -13,29 +13,45 @@ import Foundation
 
 for o in 1...5 {
     let correct = Int(arc4random_uniform(100))
-    var lastGuess = 0
-        for i in 1...5 {
-            print("Guess #\(i):?")
-            let guess = Int(readLine()!)
+    print(correct)
+    var lastGuess = correct
+        for i in 1...50 {
+            print("Guess #\(i)?")
+            let guess = Int(readLine()!)!
             var out = ""
-            if correct > 100 {
-                out = "More"
-            }
-            if correct < 100 {
-                out = "Less"
-            }
-            if correct == correct  {
-                out += " Slightly"
-            }
-            else {
-                out += " Alot"
-            }
+            
             if correct == guess {
                 print("Got it")
                 break
             }
+            if guess < correct {
+                out = "More"
+            }
+            else {
+                out = "Less"
+            }
+            
+            print(Double((guess - correct) * (guess - correct)).squareRoot())
+            print(Double(lastGuess / 2))
+            if Double((guess - correct) * (guess - correct)).squareRoot() <= Double(correct / 2 * i) {
+                out = "Slightly " + out
+            }
+            else {
+                out = "Alot " + out
+            }
+            print(out)
+            lastGuess = guess
     }
-    print("Would you like to play again??")
+    print("The answer was \(correct), Would you like to play again??")
+    var maybe = Array(readLine()!.lowercased())
+    if maybe.contains("y") {
+        if o == 5 {
+            print("This is you Fifth time playing, get a life")
+        }
+    }
+    else {
+        break
+    }
 }
 
 
